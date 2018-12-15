126c64bd3c97cb9af1ac30757c98ef33a887e7f3
Tadas Kundelis <tkundelis@csc.com>
2018-12-15 16:04:24 +0200

    added an example case

--- Maybe-monad.js
+++ Maybe-monad.js
@@ -1,19 +1,66 @@
 class Maybe {
     constructor(value) {
-      this.__value = value;
+        this.__value = value;
     }
-    static of(valueToBox){
-      return new Maybe(valueToBox);
+    static of (valueToBox) {
+        return new Maybe(valueToBox);
     }
     getOrElse(elseVal) {
-      return this.isNothing() ? elseVal : this.__value;
+        return this.isNothing() ? elseVal : this.__value;
     }
     isNothing() {
-      return this.__value === null || this.__value === undefined;
+        return this.__value === null || this.__value === undefined;
     }
-    map(fn) {  
-      return this.isNothing()?       
-               Maybe.of(null):
-               Maybe.of(fn(this.__value));
+    map(fn) {
+        return this.isNothing() ?
+            Maybe.of(null) :
+            Maybe.of(fn(this.__value));
     }
 }
+
+//dummy data array
+const users = [{
+        id: 1,
+        name: "Francois",
+        language: "French"
+    },
+    {
+        id: 2,
+        name: "Kurt",
+        language: "German"
+    },
+    {
+        id: 3,
+        name: "Peter",
+        language: null
+    },
+    {
+        null
+    }
+]
+
+
+users.forEach(getUserLanguage);
+
+function getUserLanguage(user) {
+    let language = Maybe.of(user)
+        .map(user => user.language)
+        .getOrElse("English")
+
+    console.log(language)
+}
+
+
+/*
+imperative version of the same function would be:
+
+function getUserLanguage(user) {
+    let language;
+    if (user && user.language) {
+        language = user.language;
+    } else {
+        language = "English";
+    }
+    console.log(language)
+}
+*/
