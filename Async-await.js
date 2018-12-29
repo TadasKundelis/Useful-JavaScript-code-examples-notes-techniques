ced9ceefaa70cc0e043173dfde7221a66ad32e5e
Tadas Kundelis <tkundelis@csc.com>
2018-12-30 00:52:42 +0200

    added new asynchronous code example

--- Async-await.js
+++ Async-await.js
@@ -8,8 +8,6 @@ async function getDataFromPromise() {
   console.log(result); //after 1s 'result from promise' is printed
 }
 
-getDataFromPromise();
-
 
 //get data from APi
 async function getDataFromApi(url) {
@@ -44,3 +42,56 @@ async function printInUpperCase(x) {
 
 printInUpperCase("string") //STRING 
 printInUpperCase(14) //val.toUpperCase is not a function
+
+
+//Different ways pf handling asynchronous actions
+
+function startWithDelay(timeout, name) {
+  console.log(`started ${name} `);
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(`${name} resolved after ${timeout}s`)
+    }, 1000 * timeout)
+  })
+}
+
+function concurrent() {
+  //all the functions in the array will be handled concurrently, but will wait for all promises to resolve
+  Promise.all([startWithDelay(1, 'concurrent1'), startWithDelay(4, 'concurrent2')]).then(value => console.log(value))
+}
+
+async function sequential() {
+  const res1 = await startWithDelay(2, 'sequential1'); //starts immediately
+  const res2 = await startWithDelay(3, 'sequential2'); // starts after 2 seconds
+  console.log(res1);
+  console.log(res2);
+}
+
+function parralel() {
+  //both start immediately, but first promise does not wait for the second to fnish
+  startWithDelay(1, 'parralel1').then(value => console.log(value)); //resolved after 1s
+  startWithDelay(3, 'parralel2').then(value => console.log(value)) //resolved after 3s
+}
+
+concurrent(); 
+sequential();
+parralel();
+
+/*
+Outcome: 
+
+started concurrent1 
+started concurrent2 
+started sequential1 
+started parralel1 
+started parralel2 
+
+parralel1 resolved after 1s
+started sequential2 
+parralel2 resolved after 3s
+[ 'concurrent1 resolved after 1s', 'concurrent2 resolved after 4s' ]
+sequential1 resolved after 2s
+sequential2 resolved after 3s
+*/
+
+
