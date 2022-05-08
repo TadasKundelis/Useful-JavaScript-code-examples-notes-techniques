6105e0d7c7d53e1c36ea106814cfa866dbbac712
TadasKundelis <kundelist@gmail.com>
2022-05-08 14:23:17 +0300

    structural types in typescript (#1)
    * structural types in typescript
    
    * remove empty line
    
    * remove empty line

---
+++ typescript/types.ts
@@ -0,0 +1,42 @@
+// Structural types with the same properties are interchangeable
+interface Person {
+    name: string
+    age: number
+}
+
+interface Employee {
+    name: string
+    age: number
+}
+
+function sayHello(person: Person): string {
+    return `Hello ${person.name}`
+}
+
+const employee: Employee = { name: "John", age: 35 }
+sayHello(employee) // accepts object with the same properties
+
+class Vehicle { }
+class Car extends Vehicle { }
+class Bus extends Vehicle { }
+
+const car: Bus = new Car() // not good 
+
+// Covariant arrays
+class Animal { }
+class Cat extends Animal {
+    meow(): string {
+        return "I'm a cat"
+    }
+}
+class Dog extends Animal {
+    bark(): string {
+        return "I'm a dog"
+    }
+}
+
+const cats: Cat[] = [new Cat()]
+const animals: Animal[] = cats // should not compile
+animals[0] = new Dog()
+const perhapsCat: Cat = cats[0]
+perhapsCat.meow() // TypeError: perhapsCat.meow is not a function
